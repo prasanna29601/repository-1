02f65be62acd1a1e37c7b68ea8e2002fe7fe06ec
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-08-28 18:46:25 +0530

    Add files via upload

---
+++ Area_circle.java
@@ -0,0 +1,16 @@
+package pack1;
+import java.util.*;
+public class Area_circle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Scanner obj = new Scanner(System.in);
+			//object creation or instantiation
+	System.out.println("enter radius value :");
+	float r = obj.nextFloat();
+	float a = 3.14f * r*r;
+	System.out.println("area of cicle = "+ a);
+	System.out.printf("%1.2f",a);
+	}
+
+}
