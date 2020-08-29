f8efe85440865e07bcd073b501f4eb71ad6191ef
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-08-29 20:59:45 +0530

    Add files via upload

---
+++ Static_Method.java
@@ -0,0 +1,19 @@
+package pack1;
+class Test1
+{
+	static int x =10;
+	static void show()
+	{
+		System.out.println(" static value = "+ x);
+	}
+}
+public class Static_Method {
+	
+
+	public static void main(String[] args) {
+		Test1.show();
+		// TODO Auto-generated method stub
+
+	}
+
+}
