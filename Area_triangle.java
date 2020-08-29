689de1eb04236eb9e48c36780bfed33a39293805
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-08-29 19:03:00 +0530

    Add files via upload

---
+++ Area_triangle.java
@@ -0,0 +1,17 @@
+package pack1;
+import java.util.*;
+public class Area_triangle {
+
+	public static void main(String[] args) {
+		Scanner obj=new Scanner(System.in);
+		System.out.println("Enter base and height of a triangle:");
+		int b=obj.nextInt();
+		int h=obj.nextInt();
+		float area = (b*h)/2;
+		System.out.println("Area of a Triangle= "+area);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
