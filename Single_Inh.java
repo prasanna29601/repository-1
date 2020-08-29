4da4685dcecd33f5a7c955812dac29fc4a7d9f01
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-08-29 20:59:14 +0530

    Add files via upload

---
+++ Single_Inh.java
@@ -0,0 +1,42 @@
+package pack1;
+class Student2
+{
+	String name;
+	int roll;
+    Student2(String name , int rollno)  // constructor
+    {
+    	this.name = name;
+    	roll = rollno;
+    }
+    void showStudent2()
+    {
+    	System.out.println("name = "+name+ "  roll = "+roll);
+    }
+}
+
+class Marks2 extends Student2
+{
+	int m1, m2;
+	Marks2(String name,int rn,int m1,int m2)
+	{
+	  super(name,rn);
+	  this.m1 = m1;
+	  this.m2 = m2;
+	}
+	void showmarks()
+	{
+		System.out.println("m1 = "+m1+ "  m2 = "+m2);
+	}
+}
+
+public class Single_Inh {
+
+	public static void main(String[] args) {
+		Marks2 obj = new Marks2("sai",11,56,67);
+	       obj.showStudent2();
+	       obj.showmarks();
+		// TODO Auto-generated method stub
+
+	}
+
+}
