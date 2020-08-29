d3fb3fc7bc66d20909c861a60a9e44b336f7b314
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-08-29 19:02:28 +0530

    Add files via upload

---
+++ Area_square.java
@@ -0,0 +1,15 @@
+package pack1;
+import java.util.Scanner;
+public class Area_square {
+
+	public static void main(String[] args) {
+		Scanner obj = new Scanner(System.in);
+		System.out.println("Enter the side of a square: ");
+		int s=obj.nextInt();
+		int area=s*s;
+		System.out.println("Area of a square= "+area);
+		// TODO Auto-generated method stub
+
+	}
+
+}
