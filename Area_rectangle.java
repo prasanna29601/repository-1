40cb4b76a88877e5d28d9e4c89fff5274a65ab66
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-08-29 19:01:45 +0530

    Add files via upload

---
+++ Area_rectangle.java
@@ -0,0 +1,18 @@
+package pack1;
+
+import java.util.Scanner;
+
+public class Area_rectangle {
+
+	public static void main(String[] args) {
+		Scanner obj = new Scanner(System.in);
+		System.out.println("Enter the length and breadth of rectangle: ");
+		int l=obj.nextInt();
+		int b=obj.nextInt();
+		int area=l*b;
+		System.out.println("Area of rectangle= "+area);
+		// TODO Auto-generated method stub
+
+	}
+
+}
