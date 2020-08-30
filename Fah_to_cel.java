4717af9fefce52105d631595fd0b6e0b70ca7865
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-08-30 17:39:28 +0530

    Add files via upload

---
+++ Fah_to_cel.java
@@ -0,0 +1,16 @@
+package pack1;
+import java.util.*;
+public class Fah_to_cel {
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		  float f,c;
+		  System.out.println("enter Farenheit temperature:");
+		  f = sc.nextFloat();
+		  c = ((f-32)*5)/9;
+		  System.out.println("Celsius = "+c);
+		// TODO Auto-generated method stub
+
+	}
+
+}
