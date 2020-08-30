8eb3d3055bee0ede2615d8d4f865b04d9c824545
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-08-30 17:45:30 +0530

    Add files via upload

---
+++ Even_odd_count.java
@@ -0,0 +1,30 @@
+package pack1;
+import java.util.*;
+public class Even_odd_count {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		  System.out.println("enter size :");
+		  int s = sc.nextInt();  // 8
+		  int[] nums = new int[s];
+		  int even=0, odd=0;
+		  System.out.println("enter " + s + " values:");
+		  for(int i=0; i<s ; i++)
+		  {
+			  nums[i] = sc.nextInt();  // 8 values(1 2 3 6 7 8 9 5)
+		  }
+		  
+		  for(int i = 0; i<s; i++)
+		  {
+			  if(nums[i]%2==0)
+				  even++;   // 3
+			  else
+				  odd++;  // 5
+		  }
+		  System.out.println("even number count = "+even );
+		  System.out.println("odd number count = "+odd );
+		// TODO Auto-generated method stub
+
+	}
+
+}
