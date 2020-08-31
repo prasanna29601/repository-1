55a8f2a9a5aca2b158e87aa5fb0d5705330543cc
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-08-31 16:51:19 +0530

    Add files via upload

---
+++ Area_Heptagon.java
@@ -0,0 +1,13 @@
+package Area;
+import java.util.*;
+public class Area_Heptagon {
+
+	public static void main(String[] args) {
+		 Scanner obj = new Scanner(System.in);
+	      System.out.println("enter length of a side ");
+	      float s = obj.nextFloat();
+	      float a = 3.634f * s * s;
+	      System.out.println("Area of Heptagon = "+ a);
+	}
+
+}
