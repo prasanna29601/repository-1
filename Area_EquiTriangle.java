e3d9e132416980886dd4664651dd6437f8938eab
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-08-31 16:40:54 +0530

    Add files via upload

---
+++ Area_EquiTriangle.java
@@ -0,0 +1,16 @@
+package Area;
+import java.util.*;
+public class Area_EquiTriangle {
+
+	public static void main(String[] args) {
+
+		Scanner obj = new Scanner(System.in);
+		System.out.println("enter side of a triangle:");
+		float s = obj.nextFloat();		
+		float a = (1.73f * s *s )/4;		
+		System.out.println("Area of Equilateral Triangle = "+a);
+		// TODO Auto-generated method stub
+
+	}
+
+}
