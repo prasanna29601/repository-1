00182386c97645ef0560ef59b31238dab2d1cc18
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-09-01 16:34:58 +0530

    Add files via upload

---
+++ Volume_Cone.java
@@ -0,0 +1,15 @@
+package Area;
+import java.util.*;
+public class Volume_Cone {
+
+	public static void main(String[] args) {
+		Scanner obj = new Scanner(System.in);
+		System.out.println("enter radius:");
+		int r = obj.nextInt();
+		System.out.println("enter height:");
+		int h = obj.nextInt();	
+		double v = 3.14 * (r*r)*h / 3;
+	    System.out.println("Volume of cone = "+ v);
+	}
+
+}
