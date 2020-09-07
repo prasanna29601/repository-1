c80a334dc8ce52ddd05e38043971fc8827922236
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-09-07 17:08:30 +0530

    Add files via upload

---
+++ Sum_digits.java
@@ -0,0 +1,19 @@
+package pack2;
+import java.util.*;
+public class Sum_digits {
+
+	public static void main(String[] args) {
+		Scanner obj = new Scanner(System.in);
+		  System.out.println("enter number :");
+		  int n = obj.nextInt();  
+		  int sum = 0;
+		  while( n!=0)  
+		  {
+			  int rem = n%10;  
+			  sum = sum + rem; 
+			  n = n/10 ; 
+		  }
+			System.out.println("sum of digits = "+sum); 
+	}
+
+}
