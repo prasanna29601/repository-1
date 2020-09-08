3161e67d1f3f7ecef1eb2bf7e9bd8c89656d32f5
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-09-08 16:47:31 +0530

    Add files via upload

---
+++ Sum_Even_Odd_digits.java
@@ -0,0 +1,24 @@
+package pack2;
+import java.util.*;
+public class Sum_Even_Odd_digits {
+
+	public static void main(String[] args) {
+		Scanner obj = new Scanner (System.in);
+		  System.out.println("enter number :");
+		  int n= obj.nextInt(); 
+		  int even_sum =0,  odd_sum =0;
+		  while(n!=0)  
+		  {
+			  int rem = n%10;  
+			  if (rem%2 == 0)  
+			    even_sum = even_sum+rem; 
+			  else 
+				  odd_sum = odd_sum + rem; 
+			  
+			  n = n/10;
+	  	  }
+		 System.out.println("even sum = "+ even_sum);
+		 System.out.println("odd sum = "+ odd_sum);
+	}
+
+}
