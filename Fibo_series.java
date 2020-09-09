cf7b8c4fb62342af0b13cae624c15fb53bccdef8
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-09-09 20:51:44 +0530

    Add files via upload

--- Fibo_series.java
+++ Fibo_series.java
@@ -5,17 +5,17 @@ public class Fibo_series {
 	public static void main(String[] args) {
 		Scanner obj = new Scanner(System.in);
 		System.out.println("enter number :");
-		int n = obj.nextInt(); // n = 10
+		int n = obj.nextInt(); 
 		int t1 = 0 , t2 = 1;
 		System.out.println("Fibonacci numbers upto given number:"+n);
-		System.out.print(t1);  // t1 = 0  
+		System.out.print(t1);    
 		int sum=0;
-		for(int i = 2; sum<n; i++) // 2<10
+		for(int i = 2; sum<n; i++) 
 		{	
-			sum = t1 + t2; // sum = 0+1 = 1
-			t1 = t2;  // t1 = 1
-			t2 = sum;  // t2 = 1		
-			System.out.print(" "+t1);  //0 1 1 2
+			sum = t1 + t2; 
+			t1 = t2; 
+			t2 = sum;  		
+			System.out.print(" "+t1);  
 		}
 	}
 
