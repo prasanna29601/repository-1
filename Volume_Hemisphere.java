ed1e47726f8d36224d27347d84d771fcb56f1bf1
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-09-10 20:15:11 +0530

    Add files via upload

---
+++ Volume_Hemisphere.java
@@ -0,0 +1,13 @@
+package Area;
+import java.util.*;
+public class Volume_Hemisphere {
+
+	public static void main(String[] args) {
+		Scanner obj = new Scanner(System.in);
+		 System.out.println("enter radius :");
+		 int r = obj.nextInt();
+		 double v = (2.0/3.0)*(3.14)*(r*r*r);
+	System.out.println("Volume of Hemisphere = "+v);
+	}
+
+}
