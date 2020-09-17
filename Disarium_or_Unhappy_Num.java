290a8d31c4ba5fbfe803dd481cd221c274e977f1
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-09-17 20:14:54 +0530

    Add files via upload

---
+++ Disarium_or_Unhappy_Num.java
@@ -0,0 +1,35 @@
+package pack3;
+import java.util.*;
+public class Disarium_or_Unhappy_Num {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter number :");
+		int num = sc.nextInt(); 
+		int n = num; 
+		int len = count(num);
+		int sum =0;
+		while(num>0)  
+		{
+			int rem = num%10; 
+			sum = sum + (int)Math.pow(rem, len);
+			num = num/10; 
+			len-- ; 
+		}
+		if(sum==n)
+			System.out.println(n+" is Disarium Number");
+		else
+			System.out.println(n+" is Not Disarium Number");
+	}
+   static int count(int n)
+   {
+	   int length = 0;
+	   while(n !=0 ) 
+	   {
+		   length = length + 1;
+		   n = n/10; 
+	   }
+	   return length; 
+	}
+
+}
