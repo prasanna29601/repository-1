f432a5d744c50dd733a8be84f6b1ac52e65bfe9a
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-09-18 20:06:48 +0530

    Add files via upload

---
+++ Smith_Number.java
@@ -0,0 +1,68 @@
+package pack3;
+import java.util.*;
+public class Smith_Number {
+	static int sumdigits(int n)
+	{
+		int s=0;
+		while(n>0) 
+		{
+			int rem = n%10; 
+			s = s+rem; 
+			n=n/10; 
+		}
+		return s; 
+	}
+	static int sumprimefactors(int n)
+	{
+		int i=2, sum =0;
+		
+		while(n>1)
+		{
+			if(n%i==0) 
+			{
+				sum = sum + sumdigits(i); 
+				n= n/i; 
+			}
+			else
+				i++; 
+		}
+		return sum; 
+		
+	}
+	
+	static boolean Composite(int n) 
+	{
+		int c=0;
+		for(int i=1; i<=n; i++)
+		{
+			if(n%i == 0)
+			{
+				c++; 
+			}
+		}
+		if(c>2) 
+			return true;
+		else
+			return false; 
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter number");
+		int n= sc.nextInt(); 
+		
+		if(Composite(n)==false)
+			System.out.println("Enter composite number !");
+		else
+		{
+		int a = sumdigits(n);  
+		int b = sumprimefactors(n); 
+	System.out.println("sum of digits = "+a);
+	System.out.println("sum of prime factors = "+b);
+	if(a==b)
+		System.out.println(n + " is a Smith Number" );
+	else
+		System.out.println(n + " is Not Smith Number ");
+		}
+		
+	}
+}
