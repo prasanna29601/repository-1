6286f903da0b2f8d45116737932315cd56b7eeac
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-09-21 20:15:40 +0530

    Add files via upload

---
+++ Zukarman_Number.java
@@ -0,0 +1,25 @@
+package pack3;
+import java.util.*;
+public class Zukarman_Number {
+	static int display(int n)
+	{
+		int p = 1;
+		while( n != 0)
+		{
+			int rem = n%10; 
+			p = p* rem; 
+			n = n/10; 
+		} 
+		return p; 
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter number :");
+		int n = sc.nextInt(); 
+		if(n % display(n) == 0) 
+			System.out.println("Zukarman Number");
+		else
+			System.out.println("Not Zukarman Number");
+	}
+
+}
