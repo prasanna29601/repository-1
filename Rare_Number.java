e1b19cd6acf403d35680c21b776a9a1989b9eb4a
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-09-22 20:21:24 +0530

    Add files via upload

---
+++ Rare_Number.java
@@ -0,0 +1,40 @@
+package pack4;
+import java.util.*;
+public class Rare_Number {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter number :");
+		int n = sc.nextInt(); 
+		if (rare(n))  
+		System.out.println("Rare Number");
+		else
+			System.out.println("Not Rare Number");
+	}
+	
+	static int revdigits(int num) 
+	{
+		int rev =0;
+		while(num>0) 
+		{
+			int rem = num%10;   
+			rev = rev*10 + rem; 
+			num = num/10; 
+		}
+		return rev; 
+	}
+	
+	static boolean rare(int N)
+	{
+		int revN = revdigits(N); 
+		if( revN == N) 
+			 return false;
+		return sq(N+ revN)  && sq(N-revN);		
+		
+	}
+	
+	static boolean sq(double x)
+	{
+		double sr = Math.sqrt(x);
+		return (( sr - Math.floor(sr)) == 0);
+	}
+}
