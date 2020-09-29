412697ad5362c5aa76f4bc640a5e68f24d6ab024
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-09-29 21:24:43 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,13 @@
+package Patterns;
+public class Pattern2 {
+	public static void main(String[] args) {
+		for(int i =1; i<=5;i++) // rows
+		{
+			for(int j=1; j<=5;j++) // cols
+			{
+				System.out.print("*"); //*****
+			}
+			System.out.println();
+		}
+	}
+}
