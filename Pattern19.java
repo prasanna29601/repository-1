a4fb711479cefc2234a010a371170e767e1a5890
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-10-06 20:25:24 +0530

    Add files via upload

---
+++ Pattern19.java
@@ -0,0 +1,13 @@
+package Patterns;
+public class Pattern19 {
+	public static void main(String[] args) {
+		for(int i='E'; i>='A';i--) 
+		{
+			for(int j= 'A';j<=i; j++) 
+			{
+				System.out.print((char)j+" "); 
+			}
+			System.out.println(); 
+		}
+	}
+}
