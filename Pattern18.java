9169859d4527dbe42129a212b36a288959ffdf93
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-10-06 20:25:00 +0530

    Add files via upload

---
+++ Pattern18.java
@@ -0,0 +1,14 @@
+package Patterns;
+public class Pattern18 {
+	public static void main(String[] args) {
+		for(int i='A'; i<='E';i++) 
+		{
+			for(int j= 'E';j>=i; j--) 
+			{
+				System.out.print((char)i+" "); 
+			}
+			System.out.println(); //nextline
+		}
+	}
+
+}
