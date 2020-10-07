f87bb0215f8960fc94b9a06df1836dd1d7f412f6
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-10-07 21:03:47 +0530

    Add files via upload

---
+++ Pattern24.java
@@ -0,0 +1,16 @@
+package Patterns;
+public class Pattern24 {
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=1; i <= n ; i++)// 5 rows
+		{
+			for(int j= n-1; j >= i ; j--) 	
+				System.out.print(" ");
+			
+			for(int k=1; k <= i; k++) 
+				System.out.print("*"); 
+			
+			System.out.println();			
+		}
+	}
+}
