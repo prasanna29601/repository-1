237efa6ae9d3165d3a38365d9b409e1ffe92a0e5
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-10-08 20:31:22 +0530

    Add files via upload

---
+++ Pattern28.java
@@ -0,0 +1,14 @@
+package Patterns;
+public class Pattern28 {
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=1; i<=5; i++) 
+		{
+			for(int j=n-1; j>=i; j--) 
+				System.out.print(" ");
+			for(int k= 1; k<=i; k++) 
+				System.out.print((char)(k+64)); 
+			System.out.println();
+		}
+	}
+}
