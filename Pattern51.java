a28de7f4cba0d7191ab8c586102a8019d0b62e8a
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-10-13 20:43:05 +0530

    Add files via upload

---
+++ Pattern51.java
@@ -0,0 +1,18 @@
+package Patterns2;
+public class Pattern51 {
+	public static void main(String[] args) {
+		int n=4;
+		int chars = n*2-1; 
+		int space= n-1; 
+		for(int i = n; i >= 1; i--) 
+		{
+			for(int j = space; j >= i; j--)
+				System.out.print(" ");  
+			for(int k= 1; k<= chars; k++) 
+				System.out.print((char)(k+64));
+			System.out.println();
+		chars = chars-2;  		
+		}
+	}
+
+}
