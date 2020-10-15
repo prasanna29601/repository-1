a9f052dcd9bc8bb753ba33a2d06d5606a468f19e
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-10-15 20:43:04 +0530

    Add files via upload

---
+++ Pattern59.java
@@ -0,0 +1,14 @@
+package Patterns2;
+public class Pattern59 {
+	public static void main(String[] args) {
+		int n = 3;
+		for(int i = n; i >= -n; i--) 
+		{
+			for(int j=1; j<=Math.abs(i); j++)
+			 System.out.print(" "); 
+			for(int k=Math.abs(i); k<=n ;k++)
+				System.out.print(k); 
+				System.out.println();
+		}
+	}
+}
