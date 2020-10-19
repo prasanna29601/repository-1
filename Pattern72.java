6fd04a736f3f144ed54ef07984ebd9169e359418
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-10-19 20:27:14 +0530

    Add files via upload

---
+++ Pattern72.java
@@ -0,0 +1,15 @@
+package Patterns2;
+public class Pattern72 {
+	public static void main(String[] args) {
+		int n = 5;
+		for(int i = n ; i >= 1; i--) 
+		{
+			for(int j = n-1;  j >= i; j--) 
+				System.out.print(" "); 
+			for(int k = 1; k <= i; k++)
+			System.out.print((char)(k+64)+" ");
+			System.out.println();			
+		}
+	}
+
+}
