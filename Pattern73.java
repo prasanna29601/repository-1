5be82966d89de3453701d0c7679ff9398c0b8285
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-10-19 20:43:43 +0530

    Add files via upload

---
+++ Pattern73.java
@@ -0,0 +1,14 @@
+package Patterns3;
+public class Pattern73 {
+	public static void main(String[] args) {
+		int n = 3;
+		for(int i = n; i >= -n; i--)
+		{
+			for(int j=1; j <= Math.abs(i); j++) 
+			  System.out.print(" "); 
+			for(int k= n; k >= Math.abs(i); k--) 
+				 System.out.print("* "); 
+			System.out.println();		
+		}
+	}
+}
