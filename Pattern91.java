a40ae010313ab1692729280c3d69b48725eb2351
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-10-29 17:27:04 +0530

    Add files via upload

---
+++ Pattern91.java
@@ -0,0 +1,18 @@
+package Patterns3;
+public class Pattern91 {
+	public static void main(String[] args) {
+		int n = 5;
+		for(int i= 1; i<= n;i++)
+		{
+			 for(int j=1; j < n*2; j++) 
+			 {
+				 if(j <= i || j>= n*2-i) 
+					 System.out.print("*"); 
+				 else
+					 System.out.print(" "); 
+			 }
+			 System.out.println();
+		}
+	}
+
+}
