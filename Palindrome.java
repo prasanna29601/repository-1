f813d257ed1aee804dc22389f3417dc5e08c02cf
prasanna29601 <69568023+prasanna29601@users.noreply.github.com>
2020-12-03 21:33:12 +0530

    Add files via upload

--- Palindrome.java
+++ Palindrome.java
@@ -1,36 +1,18 @@
-package pack1;
+package Strings;
 import java.util.*;
 public class Palindrome {
-
 	public static void main(String[] args) {
-		Scanner sc = new Scanner (System.in);
-		System.out.println("enter size :");
-		int s = sc.nextInt(); 
-		int n[] = new int[s];
-		int r, sum, temp;
-		System.out.println("enter "+s+ " numbers :");
-		for(int i=0; i<s; i++)
-		{
-			n[i] = sc.nextInt();   
-		}	
-		for(int i=0;i<s; i++)
-		{
-			sum =0; 
-			temp = n[i];  
-			
-			while(n[i]>0) 
-			{
-				r = n[i]%10; 
-				sum = (sum*10)+r;  
-				n[i] = n[i]/10;  
-			}
-				if (temp == sum)			
-					System.out.println(temp + " is Palindrome");
-					else
-					System.out.println(temp + " is Not palindrome");
-		}	
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter string:");
+		String str = sc.next(); 
+		String rev ="";
+		int len = str.length(); 
+		for(int i = len-1; i>=0; i--)
+			rev = rev + str.charAt(i); 
+		if(str.equals(rev))
+		System.out.println("Palindrome");
+		else
+			System.out.println("Not a Palindrome");
 	}
 
 }
